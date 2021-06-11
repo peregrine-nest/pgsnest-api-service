431b146707fbc87e8707fe6cdec0e78f4c8c2de6
admiral-thrawn <sithdarklord523@gmail.com>
2021-06-11 20:52:38 +0800

    Update dependency:add Telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Telescope
+        if ($this->app->isLocal()) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
     }
 
     /**
